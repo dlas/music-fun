ff6f71a71fb054d1f5e24ddb1a08f73635c0c200
David Stafford <dstaff@kalexland.com>
2017-06-10 12:20:53 -0700

    New trick: Once we've decided on a frequency shift, throw out all the frequencies outside the epxected range and then re-run calc_score just within the frequences we care about.

--- src/code.c
+++ src/code.c
@@ -54,39 +54,22 @@ typedef struct {
  	 * Stereo, signed, 16 bit little endian PCM format */
 	short tmpdata[CHUNK_SIZE * 2];
 
-	/* A processed array with the total energy at each frequency */
-	double energy[CHUNK_SIZE];
-
-	double downsampled[DCHUNK_SIZE];
-
 	/* Input file descriptor */
 	int fd;
 
 	int history[MAX_SAMPLES];
+	double * e_history[MAX_SAMPLES];
 } STATE;
 
-void redownsample(STATE * s) {
 
-	int i;
-	for (i = 0; i < DCHUNK_SIZE; i++) {
-		s->downsampled[i] = 0;
-	}
-
-	for (i = 0; i < CHUNK_SIZE; i++) {
-		int di;
-		di = i/DOWNSAMPLE_RATIO;
-		s->downsampled[di] += s->energy[i];
-	}
-}
-
-int pickwinner(STATE * s) {
+int pickwinner(double * energies, int low_cutoff, int high_cutoff) {
 	double max = 0;
 	int maxi = 0;
 	int i;
-	for (i = 6; i < CUTOFF; i++) {
-		if (s->downsampled[i] > max) {
+	for (i = low_cutoff; i < high_cutoff; i++) {
+		if (energies[i] > max) {
 			maxi = i;
-			max = s->downsampled[i];
+			max = energies[i];
 		}
 	}
 	return maxi;
@@ -138,9 +121,10 @@ int get_data_chunk(STATE * s) {
 /*
  * This does the FFT, computing s->energy froms->tmpdata
  */
-int do_fft(STATE * s) {
+int do_fft(STATE * s, double * output) {
 
 	int i;
+	double total_energy;
 	/* Input is in stereo so we only care about every other sample */
 	for (i = 0; i < CHUNK_SIZE; i++) {
 		s->fftw_in[i] = s->tmpdata[i*1];
@@ -151,11 +135,41 @@ int do_fft(STATE * s) {
 
 	/* Now compute the energy in each frequency*/
 	for (i = 0; i < CHUNK_SIZE; i++) {
-		s->energy[i] = (s->fftw_out[i] * conj(s->fftw_out[i]));
-		
+		double e;
+
+		e = (s->fftw_out[i] * conj(s->fftw_out[i]));
+		if (i > CHUNK_SIZE/2) {
+			e = 0;
+		}
+		output[i] = e;
+		if (i > 6) {
+			total_energy+=e;
+		}
+	}
+
+	if (total_energy >0) {
+		for (i = 0; i < CHUNK_SIZE; i++) {
+			output[i]/=total_energy;
+		}
 	}
 
 	return 0;
+}
+
+double * add_e_history(double ** e_history, int n) {
+	int i;
+	e_history[n-1] = e_history[0];
+
+	for (i = 0; i < n-1; i++) {
+		e_history[i] = e_history[i+1];
+	}
+	return e_history[n-1];
+}
+void setup_e_history(double ** e_history, int n) {
+	int i;
+	for (i = 0; i < n; i++) {
+		e_history[i] = calloc(CHUNK_SIZE,  sizeof(double));
+	}
 }
 
 void add_history(int * history, int n, int new) {
@@ -169,72 +183,85 @@ void add_history(int * history, int n, int new) {
 }
 
 
-double music[] = {N_E5, N_D5s, N_E5, N_D5s, N_E5, N_B4, N_D5, N_C5, N_A4};//, N_C4, N_E4, N_A4, N_B4};
+double music[] = {N_E5, N_D5s, N_E5, N_D5s, N_E5, N_B4, N_D5, N_C5, N_A4};//,  N_C4, N_E4, N_A4, N_B4};
 
-//double music[] = {N_E5, N_E5, N_E5, N_E5, N_E5};
+double energy_in_f(double * history, double freq_shift, int i) {
+	int fidx;
+	double m;
+	//int idx;
+	double s = 0;
 
-void calc_score2(int * history, int history_n, double * score_out, double * time_shift_out, double * freq_shift_out) {
-	int t_max = LEN(music);
-	double time_shift = (double)(history_n-1) / (double)t_max;
-	double top = 0, bottom = 0;
-	double freq_shift;
-	double M = 0;
-
-	int i;
-
-	/* See notebook page 17, 19 */
-	for (i = 0; i < t_max; i++) {
-		int first = floor((double)i * time_shift);
-		int last = floor((double)(i+1) * time_shift);
-		int x;
-		assert(first >= 0);
-		assert(last < history_n);
-		for (x = first; x < last; x++) {
-			double factor = 1;
-			double h,m;
-			//h = log(history[x]);
-			//m = log(music[i]);
-			h = history[x];
-			m = music[i];
-			
-			if (x == first) {
-				factor = (double)x*time_shift- floor((double)x*time_shift);
-			} else if (x == last) {
-				factor = ceil((double)x*time_shift) - (double)x*time_shift;
-			}
-			if (factor == 0) { factor = 1.0;}
-
-			
-
-			assert(factor >=0.0);
-			assert(factor <=1.0);
-			factor /=time_shift;
-
-			//top += factor * music[i] * (double)history[x];
-			//bottom += factor * (double)(history[x]*history[x]);
-			top += factor * h*m;
-			bottom += factor*h*h;
-			M+=factor*m*m;
-		}
+	if (i >= LEN(music)) {
+		return 0;
+	}
+	m = music[i];
+	if (m == 0) {
+		return 0;
 	}
 
-	if (bottom == 0) {
-		printf("NO DATA (%i)\n", history_n);
-		*score_out = 10000000;
-		*time_shift_out = 0;
-		*freq_shift_out = 0;
+//	for (fidx = m * freq_shift; fidx < CHUNK_SIZE; fidx*=2) {
+//		s+=history[fidx];
+//	}
+	
+	fidx = m * freq_shift;
+	s = history[fidx];
+
+	/*
+	for (idx = fidx-1; idx<fidx+1; idx++) {
+
+		s += history[idx];
+	}
+	*/
+
+	if (s > 0.1) {
+		return 1;
 	} else {
-		
-		double score;
-		freq_shift = top/bottom;
-		score = freq_shift*freq_shift*bottom - 2.0*freq_shift*top + M;
-		*score_out = score;
-		*freq_shift_out = freq_shift;
-		*time_shift_out = time_shift;
+		return 0;
 	}
+
+	//printf("%i %f %i %f %i\n", i, m, fidx, s, pickwinner(history));
+	return s;
 }
 
-void calc_score(int * history, int history_n, double * score_out, double * time_shift_out, double * freq_shift_out) {
+
+/*
+void calc_score3(double ** history, int history_n, double * score_out, double *shift_time_out, double *shift_freq_out) {
+	int t_max = LEN(music);
+	double time_shift = (double)(history_n) / (double)t_max;
+	double freq_shift;
+	double score = 0;
+	double firstf;
+	int x;
+	firstf = pickwinner(history[0]);
+	if (firstf <=25) {
+		*score_out = 0;
+		return;
+	}
+	freq_shift = firstf / music[0];
+
+	for (x = 0; x < history_n; x++) {
+		double * freqs = history[x];
+		double e1, e2;
+		int mil, mih;
+		mil = (double)x/time_shift;
+		mih = (double)(x+1) / time_shift;
+		e1 = energy_in_f(freqs, freq_shift, mil);
+		e2 = energy_in_f(freqs, freq_shift, mih);
+		if (e1 > e2) {
+			score +=e1;
+		} else {
+			score+=e2;
+		}
+	}
+	
+	*score_out = score / (double)history_n;
+	*shift_time_out = time_shift;
+	*shift_freq_out = freq_shift;
+
+}
+*/
+//double music[] = {N_E5, N_E5, N_E5, N_E5, N_E5};
+void calc_score2(int * history, int history_n, double * score_out, double * time_shift_out, double * freq_shift_out) {
 	int t_max = LEN(music);
 	double time_shift = (double)(history_n) / (double)t_max;
 	double freq_shift = 0;
@@ -244,21 +271,21 @@ void calc_score(int * history, int history_n, double * score_out, double * time_
 	int i;
 	int x;
 
-	for (i = 0; i < t_max; i++) {
+	for (i = 0; i < t_max/2; i++) {
 		double m;
 		m = log(music[i]);
 		i_m+=m;
 		i_mm+=m*m;
 	}
 
-	for (i = 0; i < history_n; i++) {
+	for (i = 0; i < history_n/2; i++) {
 		double h;
 		h = log(history[i]);
 		i_h+=h;
 		i_hh+=h*h;
 	}
 
-	freq_shift = i_m/(double)(t_max) - i_h/(double)history_n;
+	freq_shift = (i_m/(double)(t_max/2) - i_h/(double)(history_n/2));
 
 	for (x = 0; x < history_n; x++) {
 		int il, ih;
@@ -272,15 +299,16 @@ void calc_score(int * history, int history_n, double * score_out, double * time_
 		disth*=disth;
 
 		if (disth > distl || ih >= t_max) {
-			score+=distl;
+			score+=distl*distl;
 		} else {
-			score+=disth;
+			score+=disth*disth;
 		}
 
 	//	printf("%i %i %i %f %f %f\n", x, il, ih, distl, disth, score);
 	}
 
 	score/=(double)history_n;
+	score+=0.000000001;
 
 	*score_out = score;
 	*freq_shift_out = freq_shift;
@@ -288,35 +316,90 @@ void calc_score(int * history, int history_n, double * score_out, double * time_
 	//printf("FS: %f %f %f %f %f %i %i %f\n", freq_shift, i_m, i_h, i_m/(double)(history_n), i_h/(double)t_max, history_n, t_max, score);
 }
 
-int check_history(int * history, int history_n) {
+void calc_range(int * history, int history_n, int *low_out, int*high_out) {
+	double i_m = 0, i_h = 0;
+	double freq_shift;
+	int t_max = LEN(music);
+
+	int i;
+
+	for (i = 0; i < t_max/2; i++) {
+		double m;
+		m = log(music[i]);
+		i_m+=m;
+	}
+
+	for (i = 0; i < history_n/2; i++) {
+		double h;
+		h = log(history[i]);
+		i_h+=h;
+	}
+
+	freq_shift = (i_m/(double)(t_max/2) - i_h/(double)(history_n/2));
+
+	*low_out = N_A4 / exp(freq_shift);
+	*high_out = N_F5 / exp(freq_shift);
+
+	//printf("FR: %f %f %i %i %i\n", freq_shift, exp(freq_shift), *low_out, *high_out, history_n);
+}
+
+void do_it(STATE* s, int blen, double *score_out, double *time_shift_out, double * freq_shift_out, int * lr, int * hr) {
+
+	int low, high;
+	int new_history[MAX_SAMPLES];
+	int i;
+	calc_range(s->history + MAX_SAMPLES - blen, blen, &low, &high);
+
+	for (i = 0; i < blen; i++) {
+		new_history[i] = pickwinner(s->e_history[MAX_SAMPLES-blen+i], low, high);
+	}
+
+	calc_score2(new_history, blen, score_out, time_shift_out, freq_shift_out);
+	*lr=low;
+	*hr=high;
+
+}
+
+
+int check_history(STATE * s) {
 	/* See notebook #2, page 17 for derivation */
 
 	double score = 0, time_shift = 0, freq_shift = 0;
 	int i;
-	double min_score = 1000000;
+	double min_score = 1000000000000;
 	double best_freq_shift, best_time_shift;
+	double ls;
 	int best_i;
+	int low, high;
+	int l,h;
 
 	
-	for (i = history_n - 40; i < history_n; i++) {
-		printf("%i ", history[i]/10);
-	}
-	printf("\n");
 
-	for (i = 9; i < 41; i++) {
+	for (i = 12; i < 40; i++) {
 
-		calc_score(history + history_n - i, i, &score, &time_shift, &freq_shift);
+		//calc_score3(history + history_n - i, i, &score, &time_shift, &freq_shift);
+		//calc_score2(s->history + MAX_SAMPLES - i, i, &score, &time_shift, &freq_shift);
+		//printf("SSS: %g %f %f\n", score, freq_shift, time_shift);
 		//printf("CANDIDATE: %f %f %f\n", score, time_shift,freq_shift);
+		do_it(s, i, &score, &time_shift, &freq_shift, &l, &h);
 		if (score < min_score) {
 			min_score = score;
 			best_freq_shift = freq_shift;
 			best_time_shift = time_shift;
 			best_i = i;
+			low=l;
+			high=h;
 		}
 	}
+	ls=log10(min_score);
+	printf("%f %f %i %i-%i ", ls, best_freq_shift, best_i, low, high);
+	for (i = 60; i < MAX_SAMPLES; i++) {
+		printf("%02i ", s->history[i]);
+	}
+	printf("\n");
 
-	if (min_score < 0.005) {
-		printf("************* BEST: %g %f %f %i***************\n\n\n", (min_score), best_freq_shift, best_time_shift, best_i);
+	if (ls < -5) {
+		printf("************* BEST: %g %f %f %i***************\n\n\n", (ls), best_freq_shift, best_time_shift, best_i);
 	}
 	return 0;
 }
@@ -324,24 +407,26 @@ void loop(STATE * s) {
 
 
 	int w;
+	double * t;
 	
 	while (1) {
 
 		get_data_chunk(s);
-		do_fft(s);
-		redownsample(s);
-		w = pickwinner(s);
-		add_history(s->history, MAX_SAMPLES, w * 10.0);
-		check_history(s->history, MAX_SAMPLES);
-		printf("%i %i\n", w, w*TIME*DOWNSAMPLE_RATIO);
+		t = add_e_history(s->e_history, MAX_SAMPLES);
+		do_fft(s, t);
+		w=pickwinner(t, 3, CUTOFF);
+		add_history(s->history, MAX_SAMPLES, w);
+		check_history(s);
 
 	}
 }
 
 int main(void) {
 	STATE s;
+	memset(&s, 0, sizeof(s));
 
 	setup_fftw(&s);
+	setup_e_history(s.e_history, MAX_SAMPLES);
 
 	s.fd = 0;
 	loop(&s);
